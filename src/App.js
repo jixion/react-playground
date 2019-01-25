6dbfcc5e18b45a5d14d11c903064f6222a656918
Jeff Burroughs <jburroughs@sparkimaginations.com>
2019-01-25 02:49:24 -0600

    Use SWAPI for display

--- src/App.js
+++ src/App.js
@@ -21,12 +21,70 @@ const floopy = <div>
 </div>;
 
 const gooby = "Jeff";
-
+const personId = "1";
 class Mine extends Component {
     render() {
         return <h1>hello {this.props.name}</h1>;
     }
 }
+
+class StarWarsApi extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date(), gooby: "asdf"};
+    }
+
+    componentDidMount() {
+        fetch("/api/people/")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: false,
+                        error
+                    })
+                }
+            )
+        ;
+    }
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Error: {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Loading...</div>;
+        } else {
+            const listItems = items.results.map((item) => {
+                return (
+                    <li key={item.name}>
+                        Name: {item.name}<br />
+                        Height: {item.mass}<br />
+                        Mass: {item.mass}<br />
+                        Hair Color: {item.hair_color}<br />
+                        Eye Color: {item.eye_color}<br />
+                        <br />
+                    </li>
+                )
+            });
+            return (
+                <div>
+                    <h1>Using the SWAPI to Star Wars People</h1>
+                    <ul>
+                        {listItems}
+                    </ul>
+                </div>
+            );
+        }
+    }
+}
+
 
 class Clock extends Component {
     constructor(props) {
@@ -63,12 +121,8 @@ class Clock extends Component {
     }
 }
 
-
 const element = <div>
-    <Mine name="jeff" />
-    {floopy}
-    <Mine name="yourNameHere" />
-    <Clock />
+    <StarWarsApi />
 </div>;
 
 class App extends Component {
