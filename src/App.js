f457f24647e20bd5e872a3a0d0e60b48d3a2c448
Jeff Burroughs <jburroughs@sparkimaginations.com>
2019-01-25 00:37:46 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,33 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
+
+const mine = 'Jeff';
+const element = <h1>Don't look at me, {mine} did it!</h1>;
+const yeOlde =
+<div className="App">
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
             Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
+        </p>
+        <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
-          >
+        >
             Learn React
-          </a>
-        </header>
-      </div>
+        </a>
+        Okay, So I havent learned React yet, but I got this working! Hey recruiters!
+    </header>
+</div>;
+
+
+class App extends Component {
+  render() {
+    return (
+      [element, yeOlde]
     );
   }
 }
