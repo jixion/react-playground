ce832193f98d4dd68db6852b5cc815ada554e183
Jeff Burroughs <jburroughs@sparkimaginations.com>
2019-01-25 01:14:50 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,32 +3,35 @@ import logo from './logo.svg';
 import './App.css';
 
 
-const mine = 'Jeff';
-const element = <h1>Don't look at me, {mine} did it!</h1>;
-const yeOlde =
-<div className="App">
-    <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-        >
-            Learn React
-        </a>
-        Okay, So I havent learned React yet, but I got this working! Hey recruiters!
-    </header>
+const name = 'Jeff';
+const leauGeaux =
+    <img src={logo} className="App-logo" alt="logo" />
+;
+const floopy = <div>
+    <h1>Dont look at me, {name} did it!</h1>
+    {leauGeaux}
+    <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+        Learn React
+    </a>
 </div>;
 
+
+
+function Mine(props) {
+    return <h1>hello {props.name}</h1>;
+}
+
+const element = <div><Mine name="jeff" />{floopy}</div>;
 
 class App extends Component {
   render() {
     return (
-      [element, yeOlde]
+        element
     );
   }
 }
